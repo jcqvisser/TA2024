c09590213fdb1ece58064af6a0d1201c7e9c6eb5
jcqvisser <jcqvisser@gmail.com>
2024-03-02 18:05:09 +0200

    generate a fastify app

---
+++ apps/kanban-be/jest.config.ts
@@ -0,0 +1,11 @@
+/* eslint-disable */
+export default {
+  displayName: 'kanban-be',
+  preset: '../../jest.preset.js',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': '@swc/jest',
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/apps/kanban-be',
+};
